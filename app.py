6c5f014872861c477521d901b0c52787e76766ac
anacanm <anacan@mangelsdorf.io>
2020-03-10 01:02:12 -0700

    Add initial GET endpoint for nlp title
    Add an initial GET endpoint that uses the spacy nlp library
    to determine a subcategory based off of the given article
    titles

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+import spacy
+nlp = spacy.load("en_core_web_sm")
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '''
-    <h1>Hello World!</h1>
-    <h3>from: The Whole Story</h3>
-    <p>You'll know us soon :)</p>
-    '''
+
+#NOTE: getTitleFromEntities currently returns whatever entity had the highest number of occurences accross all article titles passed
+def getTitleFromEntities(titles):
+    titleEntities = {}
+    for title in titles:
+        titleToAnalyze = nlp(title)
+        for entity in titleToAnalyze.ents:
+            if(entity.label_ != 'DATE'):
+                if(entity.text not in titleEntities):
+                    titleEntities[entity.text] = 1
+                else:
+                    titleEntities[entity.text]+=1
+    maxTitleEntity = (max(titleEntities, key=titleEntities.get))
+    return maxTitleEntity
+
+
+
+@app.route('/title')
+def title():
+    return jsonify(getTitleFromEntities(request.get_json()))
